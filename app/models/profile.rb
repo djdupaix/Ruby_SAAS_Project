aca98cf7d7a24b1758a1321afc6f3e5d248b30b5
djdupaix <djdupaix@outlook.com>
2016-04-21 18:07:52 +0000

    Added avatar column to profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,4 +5,7 @@ class Profile < ActiveRecord::Base
     validates :last_name, presence: true
     validates :phone_number, presence: true
     validates :contact_email, presence: true
+    
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
