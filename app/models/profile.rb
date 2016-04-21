0d34bae994a0d2d2530a4aa1e88770adb3535339
djdupaix <djdupaix@outlook.com>
2016-04-21 15:52:15 +0000

    Added profiles form and create action

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
+    
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :phone_number, presence: true
+    validates :contact_email, presence: true
 end
